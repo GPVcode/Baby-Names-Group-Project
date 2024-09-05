2fd2e896d88bda9451d5c79f3bace6570c086028
GPV <gvill005@gmail.com>
2024-09-05 15:58:11 -0700

    updated code for better readibility

--- babynames2.cpp
+++ babynames2.cpp
@@ -17,18 +17,16 @@ void process_name(ifstream& in_file, string gender, double& total)   // Bringing
    string temp;                                                      // string temp is added to due to the comma in count entry of text file.
    
    in_file >> name >> count >> temp >> percent;                      // temp is assigned the comma and values after comma until program finds space.
-   
 
    if (in_file.fail()) {   // Check for failure after each input
       cout << "Test just failed in Process_name function";
       return; 
-   } 
+   }
    if (total > 0 && gender == "Boy ") {                              // Checking for gender for output organization.
       cout << gender << "Name: " << name << " | ";
    } else if(total > 0 && gender == "Girl ") {    
       cout << gender << "Name: " << name;
    }
-
    total = total - percent;                                          // This formula helps decide when to stop processing name
 }
 
@@ -46,16 +44,14 @@ int main()
 
    while (boy_total > 0 || girl_total > 0)
    {
-   
       in_file >> rank;
-      
       if (in_file.fail()) { 
          cout << "Something failed." << rank;
-
          return 0; 
       } 
 
       cout << "Rank " << rank << " - ";
+
       process_name(in_file, gender = "Boy ", boy_total);              // Does not run once boy or girl is less than 0.
       process_name(in_file, gender = "Girl ", girl_total);
 
