c93b8ac49c3f7a4525493161e0ad93df088346a4
GPV <gvill005@gmail.com>
2024-09-05 14:50:25 -0700

    finished baby name assignment

--- babynames2.cpp
+++ babynames2.cpp
@@ -9,45 +9,57 @@ using namespace std;
    @param in_file the input stream
    @param total the total percentage that should still be processed
 */
-void process_name(ifstream& in_file, int& rank, double& total)
+void process_name(ifstream& in_file, string gender, double& total)
 {
    string name;
    int count;
    double percent;
    string temp;
-   in_file >> name >> count >> temp >> percent;
-
-   cout << name << " " << count << " " << percent << endl;
    
-   if (in_file.fail()) { return; } // Check for failure after each input
-   // if (total > 0) { cout << name << " "; }
+   in_file >> name >> count >> temp >> percent;
+   
+
+   if (in_file.fail()) {   // Check for failure after each input
+      cout << "did i fail 1?";
+      return; 
+   } 
+   if (total > 0 && gender == "Boy ") {    
+      cout << gender << "Name: " << name << " | ";
+   } else if(total > 0 && gender == "Girl ") {    
+      cout << gender << "Name: " << name;
+   }
+
    total = total - percent;
-   cout << "total: " << total;
+
 }
 
 int main()
 {  
    ifstream in_file;
-   // if (in_file.fail()) { return 0; } // Check for failure after opening
+   in_file.open("babynames_with_percentages_final2.txt"); 
+
+   if (in_file.fail()) { return 0; } // Check for failure after opening
 
    double boy_total = 50;
    double girl_total = 50;
 
+   string gender;
+   int rank;
+
    while (boy_total > 0 || girl_total > 0)
    {
-      in_file.open("babynames_with_percentages_final2.txt"); 
-      // string sLine;
-      // getline(in_file, sLine);
-      // cout << sLine.data();
-
-      int rank;
+   
       in_file >> rank;
-      if (in_file.fail()) { return 0; } 
       
-      cout << rank << " ";
-  
-      process_name(in_file, rank, boy_total);
-      // process_name(in_file, girl_total);
+      if (in_file.fail()) { 
+         cout << "Something failed." << rank;
+
+         return 0; 
+      } 
+
+      cout << "Rank " << rank << " - ";
+      process_name(in_file, gender = "Boy ", boy_total);
+      process_name(in_file, gender = "Girl ", girl_total);
 
       cout << endl;
    }
